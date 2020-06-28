731b7fc51536c88128a9ffb21c4df775b6f942ff
ahmed_bahaa <a.bahaarefaii@gmail.com>
2020-06-28 03:43:39 +0200

    adding listview and enhancments in chart

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -32,60 +32,24 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
-                  color: Colors.white,
                   elevation: 5,
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            // color: Colors.cyan,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  margin: EdgeInsets.symmetric( horizontal: 5 , vertical:  8),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      child: Padding(
                         padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            // color: Colors.cyan,
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
+                        child: FittedBox(
+                            child: Text('\$${transactions[index].amount}')),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Container(
-                            child: Text(
-                              transactions[index].title,
-                              style: Theme.of(context).textTheme.title,
-                              // style: TextStyle(
-                              //   fontWeight: FontWeight.w700,
-                              //   fontSize: 18,
-                              // ),
-                            ),
-                            // padding: EdgeInsets.all(5),
-                          ),
-                          Container(
-                            child: Text(
-                              DateFormat.yMMMd()
-                                  .format(transactions[index].date),
-                              style: TextStyle(
-                                // fontWeight: FontWeight.w700,
-                                fontSize: 15,
-                                color: Colors.blueGrey,
-                              ),
-                            ),
-                            // padding: EdgeInsets.all(5),
-                          ),
-                        ],
-                      ),
-                    ],
+                      radius: 35,
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date), 
+                    ),
                   ),
                 );
               },
@@ -94,3 +58,61 @@ class TransactionList extends StatelessWidget {
     );
   }
 }
+
+// Card(
+//                   color: Colors.white,
+//                   elevation: 5,
+//                   child: Row(
+//                     children: <Widget>[
+//                       Container(
+//                         child: Text(
+//                           '\$${transactions[index].amount.toStringAsFixed(2)}',
+//                           style: TextStyle(
+//                             fontWeight: FontWeight.bold,
+//                             fontSize: 20,
+//                             // color: Colors.cyan,
+//                             color: Theme.of(context).primaryColor,
+//                           ),
+//                         ),
+//                         margin:
+//                             EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+//                         padding: EdgeInsets.all(10),
+//                         decoration: BoxDecoration(
+//                           border: Border.all(
+//                             // color: Colors.cyan,
+//                             color: Theme.of(context).primaryColor,
+//                             width: 2,
+//                           ),
+//                         ),
+//                       ),
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: <Widget>[
+//                           Container(
+//                             child: Text(
+//                               transactions[index].title,
+//                               style: Theme.of(context).textTheme.title,
+//                               // style: TextStyle(
+//                               //   fontWeight: FontWeight.w700,
+//                               //   fontSize: 18,
+//                               // ),
+//                             ),
+//                             // padding: EdgeInsets.all(5),
+//                           ),
+//                           Container(
+//                             child: Text(
+//                               DateFormat.yMMMd()
+//                                   .format(transactions[index].date),
+//                               style: TextStyle(
+//                                 // fontWeight: FontWeight.w700,
+//                                 fontSize: 15,
+//                                 color: Colors.blueGrey,
+//                               ),
+//                             ),
+//                             // padding: EdgeInsets.all(5),
+//                           ),
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                 );
