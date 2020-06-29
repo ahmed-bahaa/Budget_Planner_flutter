116d167212f1762d1d9c4fc4b805050e52fcafc1
ahmed_bahaa <a.bahaarefaii@gmail.com>
2020-06-30 00:37:54 +0200

    Using the Device Size in Conditions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -6,12 +6,12 @@ class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final deleteTx;
 
-  TransactionList(this.transactions , this.deleteTx);
+  TransactionList(this.transactions, this.deleteTx);
 
   @override
   Widget build(BuildContext context) {
-    return  transactions.isEmpty
-          ? LayoutBuilder(builder: ( ctx , constraints ) {
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
             return Column(
               children: <Widget>[
                 Text("No transactions added yet!",
@@ -29,42 +29,48 @@ class TransactionList extends StatelessWidget {
                 )
               ],
             );
-          }) 
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      child: Padding(
-                        padding: EdgeInsets.all(10),
-                        child: FittedBox(
-                            child: Text(
-                          '\$${transactions[index].amount}',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        )),
-                      ),
-                      radius: 35,
+          })
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: FittedBox(
+                          child: Text(
+                        '\$${transactions[index].amount}',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
                     ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTx(transactions[index].id),
-                    ),
+                    radius: 35,
                   ),
-                );
-              },
-              itemCount: transactions.length,
-            );
-
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: MediaQuery.of(context).size.width > 450
+                      ? FlatButton.icon(
+                          onPressed: () => deleteTx(transactions[index].id),
+                          icon: Icon(Icons.delete),
+                          label: Text("Delete"),
+                          textColor: Theme.of(context).errorColor,
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transactions[index].id),
+                        ),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
 
